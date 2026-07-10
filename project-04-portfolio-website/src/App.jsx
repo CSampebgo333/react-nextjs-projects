ef8c6a832bf4955d06cfe396984322006060b6d1
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-10 20:22:56 +0000

    Start of a new Project: project-04-portfolio-website

---
+++ project-04-portfolio-website/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h2>React is Fun!</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ project-04-portfolio-website/src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDom from "react-dom/client"
+import { StrictMode } from "react"
+import App from "./App.jsx"
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
