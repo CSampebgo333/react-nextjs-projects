5ecfb78187a8aafad78708dcf03240df7ce079ee
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-12 14:49:40 +0000

    Set Up Done

--- project-04-portfolio-website/src/App.jsx
+++ project-04-portfolio-website/src/App.jsx
@@ -1,9 +1,30 @@
+import Navbar from "./components/Navbar";
+import ScrollDots from "./components/ScrollDots";
+import Hero from "./components/Hero";
+import ProjectSection from "./components/ProjectsSection";
+import SkillsExperienceSection from "./components/SkillsExperienceSection";
+import RecommendationSection from "./components/RecommendationSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+
+import ThemeProvider from "./context/ThemeProvider";
+
+
 const App = () => {
 
   return (
-    <div>
-      React is Fun!
-    </div>
+    <ThemeProvider>
+      <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+        <Navbar />
+        <ScrollDots />
+        <div id="hero"> <Hero /> </div>
+        <div id="project"> <ProjectSection /> </div>
+        <div id="skill"> <SkillsExperienceSection /> </div>
+        <div id="recommendation"> <RecommendationSection /> </div>
+        <div id="contact"> <ContactSection /> </div>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ project-04-portfolio-website/src/components/RecommendationSection/index.jsx
@@ -0,0 +1,13 @@
+import ArrowNavigation from "./ArrowNavigation";
+import CompanyLogo from "./CompanyLogo";
+import TestimonialCard from "./TestimonialCard";
+
+const RecommendationSection = () => {
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default RecommendationSection;
