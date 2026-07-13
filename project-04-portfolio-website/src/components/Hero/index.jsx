97c22bd1ef2f6c8f4378b3ed97478fa5f30a0c88
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-13 15:26:18 +0000

    Build and Style the Scroll dots to visualy indicate the part of the portfolio currenyly viewed

--- project-04-portfolio-website/src/components/Hero/index.jsx
+++ project-04-portfolio-website/src/components/Hero/index.jsx
@@ -1,16 +1,7 @@
 const Hero = () => {
     return (
-        <section className="min-h-screen pt-24">
+        <section className="min-h-screen pt-24 flex flex-col md:flex-row">
             <h1> Hero Section </h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nunc risus. 
-                Morbi consectetur fusce hendrerit neque eget lectus varius ultricies. 
-                Ut vitae massa finibus, pellentesque justo mauris feugiat ut nunc eget tempor. 
-                Etiam sapien neque, dignissim sed molestie ut, placerat lorem pellentesque 
-                habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-                Ut euismod sem ac nulla euismod bibendum. Pellentesque sit amet placerat tortor. 
-                Nullam eros.
-            </p>
         </section>
     );
 }
--- project-04-portfolio-website/src/components/Navbar/index.jsx
+++ project-04-portfolio-website/src/components/Navbar/index.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { useTheme } from "../../context/useTheme";
-import { Sun, Moon } from "lucide-react";
+import { Sun, Moon, Menu, X } from "lucide-react";
 
 const Navbar = () => {
 
     const { theme, toggleTheme } = useTheme();
+
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const navLinks = [
         { label: "Home", target: "hero" },
@@ -33,7 +36,28 @@ const Navbar = () => {
                     {theme === "dark"? <Sun size={20} /> : <Moon size={20}/>}
                 </button>
                 </div>
+                <button onClick={() => setMenuOpen((prevMenu) => !prevMenu)}
+                    className="md:hidden p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 transition cursor-pointer">
+                    {menuOpen? <X size={20}/> : <Menu size={20}/>}
+                </button>
             </div>
+            {menuOpen && (
+                <div className="md:hidden flex flex-col items-starts px-4 pb-6 space-y-4">
+                    {navLinks.map(({ label, target }) => (
+                        <a key={label}
+                            href={`#${target}`}
+                            onClick={() => setMenuOpen(false)}
+                            className="transition hover:text-blue-500">
+                            {label}
+                        </a>
+                    ))}
+                    <button onClick={toggleTheme}
+                        className="p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 transition cursor-pointer">
+                        {theme === "dark" ? <Sun size={20} /> : <Moon size={20} />}
+                    </button>
+                </div>
+            )}
+            
         </nav>
     );
 }
