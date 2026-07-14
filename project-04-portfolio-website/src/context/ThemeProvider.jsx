9d5afb80a7e23f78f2e9ac20b174abe9bc4850db
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-14 23:17:46 +0000

    Just made the theme persistant

--- project-04-portfolio-website/src/context/ThemeProvider.jsx
+++ project-04-portfolio-website/src/context/ThemeProvider.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import { ThemeContext } from "./ThemeContext";
 
+
 const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState("light");
+
+    const getInitialTheme = () => (
+        localStorage.getItem("theme") || "light"
+    );
+
+    const [theme, setTheme] = useState(getInitialTheme);
 
     document.documentElement.classList.add(theme);
 
@@ -11,6 +17,7 @@ const ThemeProvider = ({ children }) => {
             const newTheme = prevTheme === "dark" ? "light" : "dark";
             document.documentElement.classList.remove("light", "dark");
             document.documentElement.classList.add(newTheme);
+            localStorage.setItem("theme", newTheme); 
             return newTheme;
         });
     };
