6c7660fdabc656be32ab68b05c4fe434643efefc
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-15 11:23:52 +0000

    Accessing the DOM node with Ref

--- project-05-learning-Ref/src/App.jsx
+++ project-05-learning-Ref/src/App.jsx
@@ -1,47 +1,18 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 
 const App = () => {
 
-  const [time, setTime] = useState(0);
+  const inputRef = useRef(null);
 
-  let intervalId = useRef(null);
-
-  const handleStart = () => {
-
-    if (intervalId.current !== null) return;
-
-    intervalId.current = setInterval (() => {
-      setTime ((prevTime) => prevTime + 10);
-    }, 10)
+  function handleClick () {
+    inputRef.current.focus();
   }
 
-  const handleEnd = () => {
-    clearInterval(intervalId.current);
-    intervalId.current = null;
-
-  }
-
- const handleReset = () => {
-    handleEnd();
-    setTime(0);
-  }
-
-  const minutes = Math.floor(time / 60000);
-  const seconds = Math.floor((time % 60000) / 1000);
-  const milliseconds = Math.floor((time % 1000) / 10);
-
-  const formattedMinutes = String(minutes).padStart(2, "0");
-  const formattedSeconds = String(seconds).padStart(2, "0");
-  const formatedMilliseconds = String(milliseconds).padStart(2, "0");
-  
   return (
     <div>
-      <h1>Timer: {formattedMinutes}:{formattedSeconds}.{formatedMilliseconds}</h1>
-      <div style={{marginTop: "50px", display: "flex", gap: "7px"}}>
-        <button style={{cursor: "pointer"}} onClick={handleStart}>Start</button>
-        <button style={{cursor: "pointer"}} onClick={handleEnd}>End</button>
-        <button style={{cursor: "pointer"}} onClick={handleReset}>Reset</button>
-      </div>
+      <input type="text" placeholder="Enter text" ref={inputRef}>
+      </input>
+      <button style={{cursor: "pointer"}} onClick={handleClick}>Focus the Input Field</button>
     </div>
   );
 }
