dd9dde32df4ba0692e401bf35f19defedc0b8113
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-29 14:41:17 +0000

    Installed and Configured tailwindcss, then Built the project's File Structure

--- project-05-Card-Flip-Game/src/App.jsx
+++ project-05-Card-Flip-Game/src/App.jsx
@@ -1,9 +1,24 @@
+import Settings from "./components/Settings";
+import ScoreBoard from "./components/Scoreboard";
+import ResetButton from "./components/ResetButton";
+import Graffiti from "./components/Graffiti";
+import GameBoard from "./components/GameBoard";
+
 
 const App = () => {
 
   return (
     <div>
-      <h1>React is Fun Learning!</h1>
+      <h1 className="text-3xl font-bold text-center mb-4">Memory Match Mania</h1>
+      {/* Settings: Let user include greed size and restart the game */}
+
+      {/* ScoreBoard: Contains number of moves, matches and total pairs */}
+
+      {/* ResetButton: Contains the resetButton */}
+
+      {/* GameBoard: Contains the Card */}
+
+      {/* Graffiti: Show the Wining celebration while play the Wining Song */}
     </div>
   );
 }
---
+++ project-05-Card-Flip-Game/src/components/GameBoard.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+const GameBoard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default GameBoard;
