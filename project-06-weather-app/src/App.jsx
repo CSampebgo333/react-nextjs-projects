d0f40a849bf5c011bca5609971bd8df3c25892cf
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-31 18:06:43 +0000

    Applied useEffect in a Weather App

--- project-06-weather-app/src/App.jsx
+++ project-06-weather-app/src/App.jsx
@@ -1,9 +1,56 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  
+  const [weather, setWeather] = useState();
+
+  const [error, setError] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+  const fetchWeather = async () => {
+
+    setError(null);
+    setWeather(null);
+
+    try {
+      const response = await fetch(
+        "https://api.open-meteo.com/v1/forecast?latitude=12.37&longitude=-1.53&current_weather=true"
+      )
+
+      if(!response.ok){
+        throw new Error(`HTTP error status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      setWeather(data.current_weather);
+
+    } catch (error) {
+      console.log(error.message);
+      setError("Failed to load weather details of given location!")
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(fetchWeather, 3000);
+  }, [])
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold">React is Fun!</h1>
+    <div style={{ padding: "2rem", fontFamily: "Arial", textAlign: "center" }}>
+      <h1>🌤️ Current Weather in Ouagadougou.</h1>
+      {weather && (
+      <div>
+        <p>Temperature: {weather.temperature}°C</p>
+        <p>Wind Speed: {weather.windspeed} km/h</p>
+      </div>
+      )}
+      {loading && <p>Loading...</p>}
+      {error && <p style={{color: "red"}}>{error}</p>}
     </div>
-  )
-}
+  );
+};
 
 export default App;
