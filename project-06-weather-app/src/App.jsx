0408c058727cbf6e686c10658663d37e3d70e3b9
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-31 23:06:57 +0000

    Ready for the Next Project

--- project-06-weather-app/src/App.jsx
+++ project-06-weather-app/src/App.jsx
@@ -2,18 +2,6 @@ import { useEffect } from "react";
 
 const App = () => {
 
-  useEffect(() => {
-    const handleKeyDown = (keyPressed) => {
-      if (keyPressed.key === " "){
-        console.log("Spacebar Key Pressed!");
-      }
-    }
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    }
-
-  }, [])
   return (
     <div>
       Press the "Spacebar" key to trigger an event!
