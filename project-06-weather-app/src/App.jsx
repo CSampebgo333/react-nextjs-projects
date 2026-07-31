60a12a7ff6ad082c9a0f1007e4248ef1c989003c
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-31 20:58:32 +0000

    Applied useEffect dependencies

--- project-06-weather-app/src/App.jsx
+++ project-06-weather-app/src/App.jsx
@@ -3,19 +3,27 @@ import { useState, useEffect } from "react";
 const App = () => {
   
   const [weather, setWeather] = useState();
-
   const [error, setError] = useState(null);
-
   const [loading, setLoading] = useState(true);
+  const [city, setCity] = useState("New York");
 
-  const fetchWeather = async () => {
+  const cityCoordinates = {
+    "New York": { lat: 40.71, lon: -74.01 },
+    London: { lat: 51.51, lon: -0.13 },
+    Tokyo: { lat: 35.68, lon: 139.69 },
+  };
+
+  const fetchWeather = async (cityName) => {
 
     setError(null);
     setWeather(null);
 
     try {
+
+      const {lat, lon} = cityCoordinates[cityName];
+
       const response = await fetch(
-        "https://api.open-meteo.com/v1/forecast?latitude=12.37&longitude=-1.53&current_weather=true"
+        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`
       )
 
       if(!response.ok){
@@ -35,19 +43,28 @@ const App = () => {
   }
 
   useEffect(() => {
-    setTimeout(fetchWeather, 3000);
-  }, [])
+    fetchWeather(city);
+  }, [city])
 
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial", textAlign: "center" }}>
-      <h1>🌤️ Current Weather in Ouagadougou.</h1>
+      <h1>🌤️ Current Weather in {city}.</h1>
+      {loading && <p>Loading...</p>}
+      <select 
+        value={city}
+        onChange={(element) => setCity(element.target.value)}
+        style={{fontSize: "16px", padding: "8px", marginBottom: "1rem"}}
+      >
+        <option>New York</option>
+        <option>London</option>
+        <option>Tokyo</option>
+      </select>
       {weather && (
       <div>
         <p>Temperature: {weather.temperature}°C</p>
         <p>Wind Speed: {weather.windspeed} km/h</p>
       </div>
       )}
-      {loading && <p>Loading...</p>}
       {error && <p style={{color: "red"}}>{error}</p>}
     </div>
   );
