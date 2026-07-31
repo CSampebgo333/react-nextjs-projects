1bcc8877feb217966cb015fcd6009c003ef3cc77
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-31 15:42:04 +0000

    Starting a New Project: project-06-weather-app

---
+++ project-06-weather-app/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">React is Fun!</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ project-06-weather-app/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDom from "react-dom/client";
+import { StrictMode } from "react";
+import './index.css'
+import App from './App.jsx'
+
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
