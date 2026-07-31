c1ff2a04c12a215952e33855be33331825e077bd
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-07-31 21:23:30 +0000

    Ready for Next Lessons

--- project-06-weather-app/src/App.jsx
+++ project-06-weather-app/src/App.jsx
@@ -1,16 +1,7 @@
 import { useEffect } from "react";
 
 const App = () => {
-
-  useEffect(() => {
-    const timeOutId = setTimeout(() => {
-      console.log("Timer Fired!")
-    }, 2000);
-    return () => {
-      clearTimeout(timeOutId);
-    };
-  }, []);
-
+  
   return (
     <div>
       <h1>Waiting for 2 seconds...</h1>
