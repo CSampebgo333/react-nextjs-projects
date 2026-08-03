825a7471f4fa316ec8b269d5df2030d51267cab5
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-08-03 15:07:13 +0000

    Built the Not Found Page

--- project-07-story-collection-app/src/pages/NotFound.jsx
+++ project-07-story-collection-app/src/pages/NotFound.jsx
@@ -1,6 +1,25 @@
+import { useLanguage } from "../context/LanguageContext";
+
 const NotFound = () => {
+
+    const { translate } = useLanguage();
+
     return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="text-center">
+            <h1 className="text-4xl font-bold mb-5">
+                404
+            </h1>
+            <p className="text-xl text-gray-600 mb-5">
+                {translate("notFoundTitle")}
+            </p>
+            <a
+                href="/"
+                className="text-blue-500 hover:text-blue-700 underline"
+            >
+                {translate("returnHome")}
+            </a>
+        </div>
     </div>
     );
 };
