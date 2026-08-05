277585aeebbcf1ff2fe01eb3de8211a79414a360
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-08-05 01:20:55 +0000

    ready to continue my learning journey by starting a new project: 'react-custom-hooks'

---
+++ react-custom-hooks/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl text-blue-400 font-semibold">React is Worth Learning in 2026.</h1>
+    </div>
+  );
+}
+
+export default App;
