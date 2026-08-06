818fbed1bdfaf8135b6beb4c4d4c162124f186dd
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-08-06 00:22:41 +0000

    ready to tackle higher order components

--- react-custom-hooks/src/App.jsx
+++ react-custom-hooks/src/App.jsx
@@ -1,8 +1,3 @@
-import TabList from "./components/advanced-react-patterns/Tabs/TabList";
-import { Tabs } from "./components/advanced-react-patterns/Tabs/Tabs";
-import Tab from "./components/advanced-react-patterns/Tabs/Tab";
-import TabPanel from "./components/advanced-react-patterns/Tabs/TabPanel";
-
 const App = () => {
 
     return (
@@ -12,24 +7,8 @@ const App = () => {
                 fontFamily: "sans-serif"
             }}
         >
-            <h1>Compound Components Example: Tabs</h1>
-            <Tabs defaultIndex={0}>
-                <TabList>
-                    <Tab index={1}>Home</Tab>
-                    <Tab index={2}>Ressources</Tab>
-                    <Tab index={3}>Settigns</Tab>
-                </TabList>
-                
-                <TabPanel index={1}>
-                    <p>Welcome to Home</p>
-                </TabPanel>
-                <TabPanel index={2}>
-                    <p> You are at Ressource Center</p>
-                </TabPanel>
-                <TabPanel index={3}>
-                    <p>Welcome to Settings</p>
-                </TabPanel>
-            </Tabs>
+            <h1>Higher Order Components</h1>
+            
         </div>
     )
 }
