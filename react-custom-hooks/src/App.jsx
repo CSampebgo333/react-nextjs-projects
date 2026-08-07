88f3a9d657dbb17ea57a72bb923eeb2b56c7146b
Clément Sampebgo <cl.sampebgo@gmail.com>
2026-08-07 18:12:37 +0000

    Memoizing expensive calculation

--- react-custom-hooks/src/App.jsx
+++ react-custom-hooks/src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from "react";
-import Parent from "./components/Parent";
+import { useState, useMemo } from "react";
+
+function expensiveCalculation (num) {
+    console.log("Running Expensive Function,...");
+    let result = 0;
+    for (let i = 0; i <= 1000; i++){
+        result += i % num;
+    }
+    return result;
+}
 
 const App = () => {
+
+    const [count, setCount] = useState(0);
+
+    const [input, setInput] = useState(5);
+
+    const result1 = useMemo(() => expensiveCalculation(input), [input]);
     
     return (
         <div>
-            <h1>Demo</h1>
-            <Parent />
+            <h1>Expensive Calcualtion Demo</h1>
+            <p>Input: {input}</p>
+            <p>Result: {result1}</p>
+
+            <button
+                onClick={() => setCount(count + 1)}
+            >
+                Increment Count ({count})
+            </button>
+            <br/>
+            <button
+                onClick={() => setInput(input + 1)}
+            >
+                Change Input
+            </button>
         </div>
     );
 }
